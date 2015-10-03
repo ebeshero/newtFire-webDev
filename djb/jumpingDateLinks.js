667c8231d41743ecc1e400817b1b092f013be202
David J Birnbaum <djbpitt@gmail.com>
2015-10-03 14:48:42 -0400

    Fiddled with date detection and setting

--- djb/jumpingDateLinks.js
+++ djb/jumpingDateLinks.js
@@ -14,18 +14,18 @@ function makeHrefDate() {
      *  Currently jumps to the end after the end of the semester; is this what you want?
      *  */
     var anchorLink = document.getElementById('dateRef');
-    var today = new Date().toISOString().split("T")[0];
-    //Comment out the preceding line and uncomment hte following one for testing
-    //var today = '2016-10-01';
+    //var today = new Date().toISOString().split("T")[0];
+    //Comment out the preceding line and uncomment the following one for testing
+    var today = '2015-10-02';
     var sessions = document.getElementsByTagName('tr');
-    var dates =[]
+    var dates =[];
     for (var i = 0, count = sessions.length; i < count; i++) {
         if (sessions[i].id != '') {
             dates.push(sessions[i].id.substr(1));
         }
     }
     if (dates.indexOf(today) != -1) {
-        target = today;
+        var target = today;
     } else {
         dates.reverse();
         for (i = 0, count = dates.length; i < count; i++) {
@@ -37,7 +37,11 @@ function makeHrefDate() {
         if (typeof target === 'undefined') {
             target = 'nope';
         }
-        anchorLink.setAttribute('href', '#d' + target);
     }
+    console.log('today  = ' + today + ' (type = ' + typeof today + ')');
+    console.log('target = ' + target + ' (type = ' + typeof target + ')');
+    console.log('dates = ' + dates + ' (type = ' + typeof dates + ')');
+    console.log(dates.indexOf(today));
+    anchorLink.setAttribute('href', '#d' + target);
 }
 window.addEventListener('DOMContentLoaded', makeHrefDate, false);
