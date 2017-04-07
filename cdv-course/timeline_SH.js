fe4216587cbef0ea959a2d73e50b9814da6949af
ebeshero <ebbondar@gmail.com>
2017-04-06 23:02:21 -0400

    progress on JavaScript Ex3_CDV.html

---
+++ cdv-course/timeline_SH.js
@@ -0,0 +1,52 @@
+/* 2017-04-06 ebb: JavaScript to respond to checkboxes in HTML input elements with @id attributes, to show/hide SVG elements whose @id attributes contain matching substrings. */
+window.onload = init;
+/* or window.addEventListener('DOMContentLoaded',init,false); */
+function init() {
+window.alert("hi!");
+  var checkboxSelect = document.querySelectorAll('input[id]');
+  for (var i = 0; i < checkboxSelect.length; i++) {
+  checkboxSelect[i].addEventListener('click', lineShow, true);
+    } 
+  }
+
+function lineShow() {
+    /*  window.alert("function lineShow is firing, and this.id is: " + this.id); */
+     var svgID = 'SVG_' + this.id
+{
+          /*  alert("CheckBox checked."); */
+            svgLine = document.getElementById(svgID);
+  /*svgLine.style.display = "block";*/
+  /*this.addEventListener('click', hide, true);*/
+  
+  if (svgLine.style.display == 'block') {
+        svgLine.style.display = 'none';
+    } else {
+        svgLine.style.display = 'block';
+    }
+            ;
+        } 
+    
+    
+}
+
+
+
+function hide() {
+
+ var svgID = 'SVG_' + this.id
+    {/*  alert("function hide() is firing!");*/
+    svgLine = document.getElementById(svgID);
+  svgLine.style.display = "none"; 
+  /*this.addEventListener('click', lineShowAgain, false);*/
+  }  
+}
+ 
+
+       
+    
+    
+
+
+  
+
+window.onload = init;
