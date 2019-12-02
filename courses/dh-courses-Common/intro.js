b27ccdfb000c3e8b295bf0c33b9e1f2df2cee062
Elisa Beshero-Bondar <ebbondar@gmail.com>
2019-12-02 17:10:53 -0500

    modifying JS intro examples

--- courses/dh-courses-Common/intro.js
+++ courses/dh-courses-Common/intro.js
@@ -6,12 +6,14 @@ function bunny() {
 buttons[0].addEventListener('click', changeColor,false)
 buttons[1].addEventListener('click', changeColor2, false)
 buttons[2].addEventListener('click', newFunction, false)
+buttons[3].addEventListener('click', anotherFunction, false)
 }
 
 
 function changeColor() {
 var p1 = document.getElementById("colorToggle")
 {p1.style.backgroundColor = "skyblue";}
+/* here, style is a *property*: the CSS styling of an element: you can add a CSS property after invoking style. */
 }
 
 function changeColor2() {
@@ -27,6 +29,13 @@ var li = document.getElementsByTagName('li')
    for (var i = 0, length = li.length; i < length; i++) {
         li[i].style.backgroundColor = "green";
     }  
+}
+
+function anotherFunction() {
+var twinkies = document.getElementsByClassName('stuff') 
+    for (var i = 0, length = twinkies.length; i < length; i++) {
+        document.twinkies[i].style.cssText = "text-decoration: underline; text-decoration-style: wavy;"   
+    }
 }
 
 
